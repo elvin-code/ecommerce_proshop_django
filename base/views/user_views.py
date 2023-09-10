b9ee1c1b66c2e8d15b8d6b111fed3e0e3e6d131e
elvin-code <ealvarez.dev@gmail.com>
2023-09-09 18:51:39 -0600

    ADD: request update user profile

--- base/views/user_views.py
+++ base/views/user_views.py
@@ -41,8 +41,27 @@ def registerUser(request):
         serializer = UserSerializerWithToken(user, many=False)
         return Response(serializer.data)
     except:
-        message = {'message':'User with this email already exists'}
+        message = {'detail':'User with this email already exists'}
         return Response(message, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateUserProfile(request):
+    user = request.user
+    
+    data = request.data
+    user.first_name = data['name']
+    user.username = data['email']
+    user.email = data['email']
+
+    if data['password'] != '':
+        user.password = make_password(data['password'])
+
+    user.save()
+    serializer = UserSerializerWithToken(user, many=False)
+    return Response(serializer.data)
+
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
